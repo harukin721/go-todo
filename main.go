19299a3b9582223d14659890b42290ca906c16c3
harukin <harukin@pepabo.com>
2024-05-26 17:35:11 +0900

    feat: Add Todo struct for handling todo items

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Body      string `json:"body"`
+	Done      bool   `json:"done"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
